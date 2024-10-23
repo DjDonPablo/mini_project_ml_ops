ff43ff212883853d3b55c8cace392486a987c7da
Maël Reynaud <mael.reynaud@epita.fr>
2024-10-23 16:39:03 +0200

    [main] NEYMAR

--- src/app.py
+++ src/app.py
@@ -7,12 +7,10 @@ model = joblib.load("regression.joblib")
 
 app = FastAPI()
 
-
-# http://localhost:5000/predict?size=205&nbrooms=2&garden=0
 @app.get("/predict")
 async def predict(size: int, nbrooms: int, garden: int):
     return {"prediction": model.predict([[size, nbrooms, garden]])[0]}
 
 
 if __name__ == "__main__":
-    uvicorn.run("app:app", host="0.0.0.0", port=6969, log_level="info")
+    uvicorn.run("app:app", host="0.0.0.0", port=6942, log_level="info")
